1865d67d9a3d8007de4794baa47d24167c116599
Nameet <nameetsn@gmail.com>
2025-06-22 04:11:10 +0530

    Initial commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const [jokes, setJokes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/api/jokes")
+      .then((res) => {
+        setJokes(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <>
+      <h1>Full stack</h1>
+      <p>Jokes: {jokes.length}</p>
+
+      {jokes.map((joke) => {
+        return (
+          <div key={joke.id}>
+            <h3>{joke.title}</h3>
+            <p>{joke.content}</p>
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
+export default App;
